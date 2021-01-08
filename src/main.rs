add9a57ff1afa3103996aede0e1ec74e9603221b
slay <flagg.kt@gmail.com>
2021-01-07 15:47:11 -1000

    added dictionary comparison, formatted output

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 /// Takes in an EPUB file and parses it, returning a list of Kanji  
-/// sorted by frequency  
+/// sorted by frequency.  
+/// The kanji found are compared to a dictionary from the kanjidic project.
+/// (link)[https://www.edrdg.org/wiki/index.php/KANJIDIC_Project]
+/// And finally the .txt file is formatted so that it can be easily uploaded to
+/// Anki.  
+///  
 /// Usage:  
-/// cargo run japanesebook.epub output.txt  
-///     - output is printed to `output.txt`  
+/// cargo run japanesebook.epub myfile.txt  
+///     - output is printed to `myfile.txt`  
 /// cargo run mybook.epub  
-///     - output is printed to the command line  
+///     - output is printed to 'output.txt'  
 
 #[macro_use]
 extern crate lazy_static;
@@ -14,10 +19,15 @@ extern crate regex;
 use epub::doc::EpubDoc;
 use regex::Regex;
 use std::env;
+use std::fs::File;
 use std::io::Write;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let kanjidict = "kanjidic2.xml";
 
     let book = EpubDoc::new(&args[1]);
     assert!(book.is_ok());
@@ -28,13 +38,73 @@ fn main() {
     let mut file = if !shouldprint {
         std::fs::File::create(&args[2]).unwrap()
     } else {
-        std::fs::File::create("dummy.txt").unwrap()
+        std::fs::File::create("output.txt").unwrap()
     };
 
     let mut book = book.unwrap();
 
     assert_eq!(0, book.get_current_page());
     assert_eq!("application/xhtml+xml", book.get_current_mime().unwrap());
+
+    //dictionary parsing
+    let mut kanjis: Vec<Kanji> = Vec::new();
+    let mut kanji = String::from(""); //.to_string();
+    let mut onyomi: String = "".to_string();
+    let mut kunyomi: String = "".to_string();
+    let mut meaning: String = "".to_string();
+
+    if let Ok(lines) = read_lines(kanjidict) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(mut ip) = line {
+                if ip.contains("<literal>") {
+                    kanji.push_str(&ip.remove(9).to_string());
+                }
+
+                if ip.contains("<reading r_type=\"ja_on\">") {
+                    if onyomi != "" {
+                        onyomi.push_str(", ");
+                    }
+                    onyomi.push_str(
+                        &ip.replace("<reading r_type=\"ja_on\">", "")
+                            .replace("</reading>", ""),
+                    );
+                }
+
+                if ip.contains("<reading r_type=\"ja_kun\">") {
+                    if kunyomi != "" {
+                        kunyomi.push_str(", ");
+                    }
+                    kunyomi.push_str(
+                        &ip.replace("<reading r_type=\"ja_kun\">", "")
+                            .replace("</reading>", ""),
+                    );
+                }
+
+                if ip.contains("<meaning>") {
+                    if meaning != "" {
+                        meaning.push_str(", ");
+                    }
+                    meaning.push_str(&ip.replace("<meaning>", "").replace("</meaning>", ""));
+                }
+
+                if ip.contains("</character>") {
+                    let word = &kanji;
+                    kanjis.push(Kanji {
+                        kanji: word.to_string(),
+                        onyomi: onyomi.to_string(),
+                        kunyomi: kunyomi.to_string(),
+                        meaning: meaning.to_string(),
+                    });
+                    kanji.clear();
+                    onyomi.clear();
+                    kunyomi.clear();
+                    meaning.clear();
+                }
+            }
+        }
+    }
+    //end dictionary parsing
 
     //gets frequencies of all chars in book
     let mut charsfreq: Vec<(char, u64)> = Vec::new();
@@ -68,14 +138,44 @@ fn main() {
         }
     }
 
+    //sorts by frequency
     kanjifreq.sort_by(|b, a| (a.1).cmp(&b.1));
 
-    for freq in kanjifreq.iter() {
-        if shouldprint {
-            print!("{:?}\n", freq);
-        } else {
-            write!(file, "{:?}\n", freq).expect("fail");
+    //compares dictionary to book lists
+    let mut merged_list: Vec<&Kanji> = Vec::new();
+    let mut leftovers: Vec<&(char, u64)> = Vec::new();
+
+    for kanji in &kanjifreq {
+        let mut found = false;
+
+        for dict_kanji in &kanjis {
+            if dict_kanji.kanji.contains(&kanji.0.to_string()) {
+                merged_list.push(dict_kanji);
+                found = true;
+            }
         }
+
+        if !found {
+            leftovers.push(&kanji);
+        }
+    }
+    //end comparisons
+
+    //prints to console or writes to file
+    for kanji in merged_list {
+        write!(
+            file,
+            "{}; {}; {}; {}\n",
+            kanji.kanji, kanji.meaning, kanji.onyomi, kanji.kunyomi
+        )
+        .expect("fail");
+    }
+
+    if leftovers.len() > 0 {
+        println!("These kanjis were not found in the dictionary:");
+    }
+    for kanji in leftovers {
+        println!("{:?}", kanji);
     }
 }
 
@@ -85,4 +185,22 @@ fn regex_speed_helper(text: &str) -> bool {
         static ref RE: Regex = Regex::new(r"\p{Han}").unwrap();
     }
     RE.is_match(text)
+}
+
+#[derive(Debug)]
+struct Kanji {
+    kanji: String,
+    onyomi: String,
+    kunyomi: String,
+    meaning: String,
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
